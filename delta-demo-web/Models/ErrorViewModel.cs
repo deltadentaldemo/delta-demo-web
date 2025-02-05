f20d16bbe09abab859a97c92b1489e6eba153d65
Vinod Soni <vinod.soni@microsoft.com>
2025-02-05 11:42:20 -0500

    Add project files.

---
+++ delta-demo-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace delta_demo_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
