f35dbd2914a769ace6af23101b46cd680a828ee1
Vinod Soni <vinod.soni@microsoft.com>
2025-02-05 12:34:32 -0500

    some changes

--- delta-demo-web/Models/ErrorViewModel.cs
+++ delta-demo-web/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace delta_demo_web.Models
 {
     public class ErrorViewModel
     {
+        //Some code changes
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
